0269e0e59d0e231213d16ff2ed4242a2b6e889b5
tom2u <tbrodhead@2u.com>
2020-04-14 10:17:46 -0500

    reconciled app.js with author's version of it

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
 // Notice the lack of parentheses around the `profileDataArr` parameter?
 const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
+    
+    console.log('================');
+
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 

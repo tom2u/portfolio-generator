099fb48ae5b0fca18baa4a35e9365ce92d89e702
tom2u <tbrodhead@2u.com>
2020-04-16 15:08:39 -0500

    edited code to match author's code

--- app.js
+++ app.js
@@ -193,13 +193,11 @@ var mockData =  {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    //const pageHTML = generatePage(portfolioData);
-    const pageHTML = generatePage(mockData);
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
 
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
 });
 */
